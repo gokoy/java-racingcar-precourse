e826d41634215f17571c9ca70d521370f853ce45
gokoy <gokoy50@gmail.com>
2022-04-20 11:17:04 +0900

    feat: CarDto 객체 출력 함수 생성

--- src/main/java/racingcar/view/View.java
+++ src/main/java/racingcar/view/View.java
@@ -1,6 +1,8 @@
 package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
+import racingcar.dto.CarDto;
 
 public class View {
 
@@ -13,5 +15,22 @@ public class View {
 
     public static String getCount() {
         return Console.readLine().trim();
+    }
+
+    public static void printCars(List<CarDto> dtos) {
+        System.out.println("실행 결과");
+
+        for (CarDto dto : dtos) {
+            printCar(dto);
+        }
+    }
+
+    public static void printCar(CarDto dto) {
+        StringBuilder builder = new StringBuilder();
+        builder.append(dto.getName()).append(" : ");
+        for (int i = 0; i < dto.getDistance(); i++) {
+            builder.append("-");
+        }
+        System.out.println(builder);
     }
 }
