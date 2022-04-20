7d7b93979cc6a236251d36a163e74f0069e06b3d
gokoy <gokoy50@gmail.com>
2022-04-20 10:25:44 +0900

    feat: Controller에서 View 활용하여 Domain 생성
    View에서 자동차 이름, 시도 횟수를 입력받아 Racing 객체를 생성한다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,14 @@
 package racingcar;
 
+import racingcar.domain.Racing;
+import racingcar.view.View;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String carNames = View.getCarNames();
+        String count = View.getCount();
+        Racing racing = new Racing(carNames, count);
+
     }
 }
